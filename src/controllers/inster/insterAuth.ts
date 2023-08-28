c383c1a8f5a49f48afa36e5bdfa5bf2311ad228a
Tejas Wadiwala <tejaswadiwala@gmail.com>
2023-08-27 23:56:56 -0400

    feat(INSTER): backend url

--- src/controllers/inster/insterAuth.ts
+++ src/controllers/inster/insterAuth.ts
@@ -2,7 +2,8 @@ import axios, { AxiosInstance } from 'axios'
 
 export const insterAxiosInstance: AxiosInstance = axios.create({
   // TODO: Add the below in the environment variable
-  baseURL: 'http://localhost:3300',
+  // baseURL: 'http://localhost:3300',
+  baseURL: 'https://inster-backend-aae4e333504b.herokuapp.com',
 })
 
 insterAxiosInstance.interceptors.request.use((config) => {
