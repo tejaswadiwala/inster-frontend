0b189341d9f5fee5b645250ccb79898003ab8498
Tejas Wadiwala <tejaswadiwala@gmail.com>
2023-09-08 00:31:08 -0400

    fix(INSTER): add logo

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,15 @@
 import '../css/Footer.css'
+import { Link } from 'react-router-dom'
 
 function Footer() {
   return (
     <div className="footer">
-      <p>&copy; 2023 Inster. All rights reserved.</p>
+      <p>
+        &copy; 2023 Inster. All rights reserved.{' '}
+        <Link className="footer-link" to="/privacy-policy">
+          Privacy Policy
+        </Link>
+      </p>
     </div>
   )
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,14 @@
 import '../css/Home.css'
+import insterLogo from '../assets/insterLogo.png'
 
 function Home() {
   return (
     <div className="home-container">
-      <h1 className="home-title">Welcome to Inster</h1>
+      <img
+        src={insterLogo}
+        alt="Inster Logo"
+        className="inster-logo" // Add a class for styling if needed
+      />
     </div>
   )
 }
